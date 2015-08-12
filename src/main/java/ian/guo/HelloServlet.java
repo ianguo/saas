535cf948802d832d2f90466af6de72cf3b298646
LDNS guoyl <guoyl@liandisys.com.cn>
2015-08-12 17:21:55 +0900

    rename.

--- src/main/java/ian/guo/HelloServlet.java
+++ src/main/java/ian/guo/HelloServlet.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Hello
  */
 @WebServlet("/index")
-public class Hello extends HttpServlet {
+public class HelloServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
   /**
    * @see HttpServlet#HttpServlet()
    */
-  public Hello() {
+  public HelloServlet() {
     super();
   }
 
